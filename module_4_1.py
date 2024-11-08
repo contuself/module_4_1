878c05b15fd981d599084cdd6b47adc49fe32d0a
contuself <andreyrezhepa@gmail.com>
2024-11-08 22:12:01 +0300

    Update module_4_1.py

--- module_4_1.py
+++ module_4_1.py
@@ -1,10 +1,10 @@
-from fake_math import devide as f_d
-from true_math import devide as t_d
-result1 = f_d(69, 3)
-result2 = f_d(3, 0)
-result3 = t_d(49, 7)
-result4 = t_d(15, 0)
-print(result1)
-print(result2)
-print(result3)
-print(result4)
+from fake_math import devide as f_d #импорт 
+from true_math import devide as t_d #импорт
+result1 = f_d(69, 3) #условие
+result2 = f_d(3, 0) #условие
+result3 = t_d(49, 7) #условие
+result4 = t_d(15, 0) #условие
+print(result1) #вывод на экран
+print(result2) #вывод на экран
+print(result3) #вывод на экран
+print(result4) #вывод на экран
