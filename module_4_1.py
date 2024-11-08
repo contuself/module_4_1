5e81bdda0315bdf193143313340b63e2c101afc9
contuself <andreyrezhepa@gmail.com>
2024-11-08 22:10:35 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+from fake_math import devide as f_d
+from true_math import devide as t_d
+result1 = f_d(69, 3)
+result2 = f_d(3, 0)
+result3 = t_d(49, 7)
+result4 = t_d(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
